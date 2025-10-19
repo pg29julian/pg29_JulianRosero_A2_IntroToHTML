5efa0434f456d3dcbc273cbb4b771d7b8fd0c51f
Julian <pg29julian@vfs.com>
2025-10-18 23:06:11 -0700

    [MODIFIED] changed button input handling location

--- libraries/gameEngine.js
+++ libraries/gameEngine.js
@@ -12,20 +12,6 @@ class GameEngine {
         this.gameBoard = new GameBoard();
         this.gameLogic = new GameLogic(this.gameBoard);
         this.userInput = new UserInput(this.gameLogic);
-
-        // Buttons
-
-        this.newGameButton = document.querySelector('#new_game_button');
-        this.beginnerButton = document.querySelector('#beginner_button');
-        this.mediumButton = document.querySelector('#medium_button');
-        this.expertButton = document.querySelector('#expert_button');
-
-        // Add event listeners
-
-        this.newGameButton.addEventListener('click', () => this.gameLogic.newGame(0));
-        this.beginnerButton.addEventListener('click', () => this.gameLogic.newGame(0));
-        this.mediumButton.addEventListener('click', () => this.gameLogic.newGame(1));
-        this.expertButton.addEventListener('click', () => this.gameLogic.newGame(2));
     }
 
 }
--- modules/userInput.js
+++ modules/userInput.js
@@ -4,11 +4,26 @@
 
         this.gameLogic = gameLogic;
 
+		// Input
+
 		this.boundHandlerLeftClick = this.handleLeftClick.bind(this);
 		this.boundHandleRightClick = this.handleRightClick.bind(this);
 
         this.addEventListeners();
 
+		// Buttons
+
+		this.newGameButton = document.querySelector('#new_game_button');
+		this.beginnerButton = document.querySelector('#beginner_button');
+		this.mediumButton = document.querySelector('#medium_button');
+		this.expertButton = document.querySelector('#expert_button');
+
+		// Add event listeners
+
+		this.newGameButton.addEventListener('click', () => this.gameLogic.newGame(0));
+		this.beginnerButton.addEventListener('click', () => this.gameLogic.newGame(0));
+		this.mediumButton.addEventListener('click', () => this.gameLogic.newGame(1));
+		this.expertButton.addEventListener('click', () => this.gameLogic.newGame(2));
     }
 
 	// Add event listeners
